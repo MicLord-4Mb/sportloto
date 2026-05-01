4ef8567e466e2f25582f102de8066d5d1f37b07c
MicLord <miclord@yahoo.com>
2026-05-01 23:21:52 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from 'antd';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold text-blue-600 mb-5">
+        React + Vite + Tailwind + AntD
+      </h1>
+      <Button type="primary">Ant Design Button</Button>
+    </div>
+  )
+}
+
+export default App
