6b2e9db880b2ee889a0fc217b74d46aa1e24054c
MicLord <miclord@yahoo.com>
2026-05-02 23:36:14 +0300

    add random number logic and statistic

--- src/components/layers/MainContent.jsx
+++ src/components/layers/MainContent.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import { Button, Layout, Card, Space, Select, Typography, theme as antdTheme } from 'antd';
 import { GlobalOutlined, SunOutlined, MoonOutlined, ShakeOutlined } from '@ant-design/icons';
 
+import { getStatistic } from '../../utils/math';
 import LotteryBall from '../atoms/LotteryBall';
 import StatBox from '../atoms/StatBox';
 
@@ -14,21 +15,50 @@ const MainContent = ( { isDarkMode, toggleTheme } ) => {
   const { token } = useToken();
 
   const [lang, setLang] = useState('en');
+  const [currentResult, setCurrentResult] = useState(null);
 
+  const handleGenerator = useCallback(() => {
+    // Logic to generate lottery numbers and update statistics
+
+    // Example: Generate 5 random numbers between 1 and 36
+    // const numberArray = Array.from({ length: 36 }, (_, i) => i + 1);
+    // const shuffled = numberArray.sort(() => 0.5 - Math.random());
+    // const selectedNumbers = shuffled.slice(0, 5).sort((a, b) => b - a);
+
+    const numberArray = Array.from({ length: 36 }, (_, i) => i + 1);
+    // simple shuffle
+    for (let i = numberArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [numberArray[i], numberArray[j]] = [numberArray[j], numberArray[i]];
+    }
+    // take first 5 and sort in descending order
+    const selectedNumbers = numberArray.slice(0, 5).sort((a, b) => b - a);
+
+    console.log('Generated numbers:', selectedNumbers);
+
+    const stats = getStatistic(selectedNumbers);
+    console.log('Statistics:', stats);
+
+    // TODO: make history of generated numbers and statistics, and display it in the history section
+    // Example of new entry for history
+    const newEntry = { id: Date.now(), numbers: selectedNumbers, stats };
+
+    setCurrentResult(newEntry);
+
+  }, []);
+  
   return (
       <Layout className="min-h-screen transition-colors duration-300">  {/* style={{ background: '#f9fafb' }} */}
         <div className="max-w-3xl mx-auto w-full p-4 md:p-8">
           
           {/* Header */}
-          <Card className="mb-8 shadow-sm rounded-3xl"> {/* style={{ background: '#e0f7fa' }} */}
+          <Card className="mb-8 shadow-sm rounded-3xl"> 
             <div className="flex flex-col md:flex-row items-center justify-between">
-              {/* <h1 className="text-3xl font-bold text-blue-600(155DFC) mb-5"> */}
               <Title level={3} style={{ color: `var(--ant-blue)` }} >
                 <ShakeOutlined className="mr-2" />
                 SPortLoto 5 of 36
               </Title>
               <Space>
-                {/* <div className="flex items-center gap-2 bg-gray-100 dark:bg-gray-700 p-1 rounded-lg"> */}
                 <div className="flex items-center gap-2 p-1 rounded-lg transition-colors" 
                 style={{ backgroundColor: token.colorFillAlter }}>
                   <GlobalOutlined />
@@ -50,7 +80,15 @@ const MainContent = ( { isDarkMode, toggleTheme } ) => {
 
           {/* Main Content */}
           <Card className="mb-8 shadow-sm text-center rounded-3xl" >
-            <Button type="primary">!!! Preeessss Meee !!!!</Button>
+            <Button
+              type="primary"
+              size = "large"
+              icon={<ShakeOutlined />}
+              onClick={handleGenerator}
+              className="h-14 px-8 text-lg rounded-fill"
+            >
+              !!! Preeessss Meee !!!!
+            </Button>
             <div className="flex flex-wrap justify-center gap-3 md:gap-4 mb-8">
               <LotteryBall number={1} />
               <LotteryBall number={2} />
@@ -61,11 +99,11 @@ const MainContent = ( { isDarkMode, toggleTheme } ) => {
             <Card className="mb-8 shadow-sm text-center rounded-3xl" >
               <h2 className="text-2xl font-semibold text-gray-800 mb-4">Statistics</h2>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mb-6">
-                <StatBox label="Min value" value={5} />
-                <StatBox label="Max value" value={30} />
-                <StatBox label="Average" value={18.4} />
-                <StatBox label="Even count" value={3} color="green" />
-                <StatBox label="Odd count" value={2} color="red" />
+                <StatBox label="Min value" value={currentResult?.stats.min} />
+                <StatBox label="Max value" value={currentResult?.stats.max} />
+                <StatBox label="Average" value={currentResult?.stats.average} />
+                <StatBox label="Even count" value={currentResult?.stats.even} color="green" />
+                <StatBox label="Odd count" value={currentResult?.stats.odd} color="red" />
               </div>
             </Card>
           </Card>
